e062319ee2f93c13b02730c992b58a93add020ea
steve hardy <plentifullack@hotmail.com>
2021-04-25 13:47:38 -0700

    Update main.cpp
    provide for a minimal usage query (-h) from the command line.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,10 @@
 /*
  Example move sequence for a winning game (using game option 'x' in my macOs environment):
  ----
- t3;t1;t4;t0;t0,2;t6;s;t3;f0;d;t2;t6;f0;s;t0;t2;t6,2;t3;t1,2;t6;t3,3;t0;t1;t5;s;s;s;d;t3;d;t3;d;t3;t1;t3;d;t1;s;s;d;t5;t1,2;t5;s;s;s;d;f1;s;s;s;d;f2;s;t1;f2;t4;f2;t4;t1;t5,5;t4;s;d;f3;s;t2;f3;t0;f3;t2;f0;t2;f2;t2;f1;t1;f2;s;d;t5;s;t0;f0;t2;t0;t2;t3;t2;t3;s;s;s;d;f1;t0;f1;t3;f1;t3;f2;t0;t1;t0;f1;t3;f1;s;d;t4;s;s;d;t6;s;s;d;t2;s;d;f3;t1;f3;s;t4;f1;t0;f1;t0;t3;t0;f0
+ s;s;d;t1;t3;t0;t6;t3;s;s;s;d;f0;s;s;s;s;d;t5;t0,2;t5;t0;t6;t2;t6;t5,4;t1;t1,6;t4;d;t1;t0;t1;t5;f0;t0;t5
+    s;s;s;s;d;f1;d;d;d;t6;t4,7;t6;s;s;s;d;t0;t1,3;t0;t1;t3;t1;f2;d;f2;s;d;f3;s;s;s;s;s;d;t0;s;d;t0;t3,3;t0
+    t1;f3;t1;f0;t0,10;t1;t2;f0;s;s;s;d;f2;t6;f2;s;d;t0;t3;t0;t4;t3;s;d;t5;t0,3;t4;t0;f0;s;t6;t2;t6;f0;t2,2;t6
+    d;t5;s;d;f3;s;d;f1;s;d;t0;s;t1;f3;t6;f3;t1;f2;t1;f3;t1;f0;d;f3;s;d;f0;t2;f0;s
  ----
  */
 int main(int argc, const char * argv[])
@@ -21,17 +24,23 @@ int main(int argc, const char * argv[])
     //
     // To select a number of initial shuffles (e.g., 3), presumably for a winnable ordering, use argument 'x3'.
     //
-    int shufflect = 1;
-    bool randomize_shuffle = argc < 2 || 'x' != *argv[1];
-    if (randomize_shuffle) {
-        std::srand((unsigned int)std::time(0));
-    } else if ('x'==*argv[1] && isdigit(argv[1][1])) {
-        shufflect = atoi(&argv[1][1]);
+    if (argc==2 && 0==strcmp(argv[1], "-h")){
+        std::cout << "\nSolitaire game\n\nusage:\n\tsolitaire [xN|-h]\n"
+        <<"  where\n \t x derandomizes initial shuffle. (N is a small positive number mapping to a repeatable state on current platform)"
+        <<"\n\t-h shows this help and exits\n"
+        << std::endl;
     }
-    // std::cout << "randomize: " << randomize_shuffle << std::endl;
-    Game g;
-
-    Deck d2(randomize_shuffle);
-    g.start(d2.shuffle().shuffle(shufflect));
+    else{
+        int shufflect = 1;
+        bool randomize_shuffle = argc < 2 || 'x' != *argv[1];
+        if (randomize_shuffle) {
+            std::srand((unsigned int)std::time(0));
+        } else if ('x'==*argv[1] && isdigit(argv[1][1])) {
+            shufflect = atoi(&argv[1][1]);
+        }
+        Game g;
+        Deck d2(randomize_shuffle);
+        g.start(d2.shuffle().shuffle(shufflect));
+    }
     return 0;
 }
